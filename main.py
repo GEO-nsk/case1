8f15b00c8125c9889067943a94e0981c62c82f0b
Evgeny <e.tarlo@g.nsu.ru>
2023-03-02 19:53:05 +0700

    Circle

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ Gagol Egor
 Tarlo Evgeny
 Karpenko Nikolay
 '''
-
+import turtle
 from turtle import *
 t = Turtle()
 t.screen.setup(900, 800)
@@ -15,6 +15,15 @@ t.screen.setup(900, 800)
 
 
 # Evgeny
+
+size = int(input())
+def circle(size):
+    for i in range (360):
+        fd(size / 10)
+        rt(1)
+
+circle(size)
+turtle.done()
 
 
 
