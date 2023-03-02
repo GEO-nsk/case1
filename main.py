e311f185a1d60f15b351b7928fc6b4d64af199c3
Evgeny <e.tarlo@g.nsu.ru>
2023-03-03 01:22:34 +0700

    Added lines for dividing

--- main.py
+++ main.py
@@ -7,27 +7,39 @@ Karpenko Nikolay
 '''
 
 from turtle import *
-t = Turtle()
-t.screen.setup(900, 800)
+speed(0)  # Drawing speed (0 - the fastest)
+
+line_lenght = 900
+# lines for dividing the drawing space into 3 parts
+def line(line_lenght):
+    pd()
+    fd(line_lenght)
+    pu()
+
+
 pu()
-lt(180)
+lt(90)
 fd(450)
-rt(180)
+lt(90)
+fd(150)
+lt(90)
+line(line_lenght)
+lt(90)
+fd(300)
+lt(90)
+line(line_lenght)
+
+# Return to the starting point
+setx(0)
+sety(0)
+rt(90)
+
 
 # Egor
 
 
 # Evgeny
 
-def rectangle(w, h):
-    for i in range(2):
-        fd(w)
-        lt(90)
-        fd(h)
-        lt(90)
-
-
-done()
-
 
 # Nikolay
+
