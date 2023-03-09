9133be7df6b79f60d2f22c488313ee1e0cd1c184
Evgeny <e.tarlo@g.nsu.ru>
2023-03-09 11:55:32 +0700

    BIBA

--- main.py
+++ main.py
@@ -7,32 +7,9 @@ Karpenko Nikolay
 '''
 
 from turtle import *
-speed(0)  # Drawing speed (0 - the fastest)
-
-# function to draw a line
-line_lenght = 900
-def line(line_lenght):
-    pd()
-    fd(line_lenght)
-    pu()
-
-# lines for dividing the drawing space into 3 parts
-pu()
-lt(90)
-fd(450)
-lt(90)
-fd(150)
-lt(90)
-line(line_lenght)
-lt(90)
-fd(300)
-lt(90)
-line(line_lenght)
-
-# Return to the starting point
-setx(0)
-sety(0)
-rt(90)
+from math import *
+# Drawing speed (0 - the fastest)
+speed(0)
 
 
 # Egor
@@ -41,12 +18,17 @@ rt(90)
 # Evgeny
 
 # function to draw a rectangle
-def rectangle(w, h):
-    for i in range(2):
-        fd(w)
-        lt(90)
-        fd(h)
-        lt(90)
+# side - left and right sides
+# base - the base
+# angle - the angle under which you need to draw
+# corner - the corner between side and base
+# border_color - the color of border
+# fill_color - the color of fill
+def triangle(side, base, angle, corner, border_color, fill_color):
+    rt(angle)
+    fd(side)
+
+
 
 
 # Nikolay
