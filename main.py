9c0bb4dff39d8740875d574b9362fdc53beed255
Егор <geo-04@mail.ru>
2023-03-11 12:04:21 +0700

    commit 3e

--- main.py
+++ main.py
@@ -38,6 +38,14 @@ def draw_rectangle(width, length, angle, border_color, fill_color):
     pu()
     goto(0,0)
 
+def draw_circle(radius, border_color, fill_color):
+    pd()
+    begin_fill()
+    color(border_color, fill_color)
+    circle(radius)
+    end_fill()
+    pu()
+    goto(0,0)
 
 # Evgeny
 def triangle_exist(a, b, c):
@@ -92,25 +100,54 @@ Start of drawing
 # Egor
 
 rt(270)
-# # Square1
-draw_rectangle(75,75,0,'blue1','brown')
-# Square 2
+# Truck
+goto(250,0)
+lt(45)
+draw_triangle(sqrt(20000),200,sqrt(20000),'black','Black')
+rt(45)
+draw_rectangle(75,75,0,'Black','Black')
 goto(-100,0)
-draw_rectangle(100,100,0,'blue1','brown')
-# Square 3
+draw_rectangle(100,100,0,'Black','DarkRed')
 goto(-100,100)
-draw_rectangle(100,100,0,'azure3','azure3')
+draw_rectangle(100,100,0,'Black','azure3')
 goto(-170,100)
 rt(90)
-draw_triangle(100, sqrt(100**2 + 70**2), 70,'azure3','azure3')
+draw_triangle(100, sqrt(100**2 + 70**2), 70,'Black','azure3')
 lt(90)
 goto(-170,0)
-draw_rectangle(100,70,0,'black','black')
+draw_rectangle(100,70,0,'black','DarkRed')
 goto(20,75)
-draw_rectangle(100,200,0,'blue1','brown')
+draw_rectangle(100,200,0,'Black','Gold2')
 goto(0,-30)
-draw_rectangle(30,250,0,'grey1','grey1')
-goto(50,0)
+draw_rectangle(30,250,0,'Black','Black')
+goto(320,175)
+lt(90)
+draw_triangle(100,sqrt(100**2 + 100**2),100,'Black','gold2')
+goto(20,150)
+rt(90)
+draw_triangle(100,sqrt(100**2 + 100**2),100,'Black','gold2')
+lt(90)
+goto(-155,50)
+# wheels
+draw_circle(15,'Black','gold')
+goto(-90,35)
+draw_circle(50,'Black','gray50')
+goto(230,35)
+draw_circle(50,'Black','gray50')
+goto(115,35)
+draw_circle(50,'Black','gray50')
+goto(-90,5)
+draw_circle(20,'Black','gray80')
+goto(230,5)
+draw_circle(20,'Black','gray80')
+goto(115,5)
+draw_circle(20,'Black','gray80')
+goto(-90,-5)
+draw_circle(10,'Black','Black')
+goto(230,-5)
+draw_circle(10,'Black','Black')
+goto(115,-5)
+draw_circle(10,'Black','Black')
 
 
 # Evgeny
